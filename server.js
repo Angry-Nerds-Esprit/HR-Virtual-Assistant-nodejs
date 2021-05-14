505bd2053daed16f39b1080270172cb2429ffebe
sofientr <trikisofien03@gmail.com>
2021-05-14 01:42:01 +0100

    deploy config

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 ﻿require('rootpath')();
+require('dotenv').config({path: __dirname + '/.env'})
 const express = require('express');
 const app = express();
 const cors = require('cors');
@@ -6,9 +7,11 @@ const bodyParser = require('body-parser');
 const jwt = require('_helpers/jwt');
 const errorHandler = require('_helpers/error-handler');
 const ImageUploadRouter = require("./cv/route/uploadImageRoute");
+const path= require('path');
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json()); 
 app.use(cors());
+
 
 // use JWT auth to secure the api
 app.use(jwt());
@@ -27,7 +30,14 @@ app.use("/api", ImageUploadRouter);
 //app.use(errorHandler);
 
 // start server
+if(process.env.NODE_ENV === 'production'){
+    app.use(express.static(path.join(__dirname,'/client/build')))
+    app.get('*',cors,(req,res)=>{
+        res.sendFile(__dirname,'client','build','index.html')
+    });
+}
+
 const port = process.env.NODE_ENV === 'production' ? (process.env.PORT || 80) : 4000;
 const server = app.listen(port, function () {
-    console.log('Server listening on port ' + port);
+    console.log('Server listening on port ' + port +'with mode '+ process.env.NODE_ENV);
 });
